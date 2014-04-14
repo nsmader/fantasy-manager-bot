36416edc8a4147692a5b14370ef749c4852a9b81
hunterowens <owens.hunter@gmail.com>
2014-04-14 20:12:28 +0800

    make conncection now works, fixes #1

--- make_connection.py
+++ make_connection.py
@@ -1,28 +1,29 @@
-import yahoo.application
-
 # Yahoo! OAuth Credentials - http://developer.yahoo.com/dashboard/
 
-CONSUMER_KEY      = '##'
-CONSUMER_SECRET   = '##'
-APPLICATION_ID    = '##'
-CALLBACK_URL      = '##'
+# Credentials for an App hunter registered to be used only for test
 
-oauthapp      = yahoo.application.OAuthApplication(CONSUMER_KEY, CONSUMER_SECRET, APPLICATION_ID, CALLBACK_URL)
+CONSUMER_KEY      = 'dj0yJmk9clJFN2N6bTRSM3E4JmQ9WVdrOWJHSkdhamRFTm1VbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0zZQ--'
+CONSUMER_SECRET   = 'f9545904f2cc39f67104031c32ef83dfe37c6570'
+APPLICATION_ID    = 'lbFj7D6e'
+CALLBACK_URL      = 'localhost.owens.coffee'
 
-# Fetch request token
-request_token = oauthapp.get_request_token(CALLBACK_URL)
+from rauth import OAuth1Service
 
-# Redirect user to authorization url
-redirect_url  = oauthapp.get_authorization_url(request_token)
+yahoo = OAuth1Service( 
+	consumer_key=CONSUMER_KEY,
+	consumer_secret=CONSUMER_SECRET, 
+	name='yahoo', access_token_url= 'https://api.login.yahoo.com/oauth/v2/get_token',
+	authorize_url= "https://api.login.yahoo.com/oauth/v2/request_auth"	, 
+	request_token_url= "https://api.login.yahoo.com/oauth/v2/get_request_token",
+	base_url='https://api.login.yahoo.com/oauth/v2/')
 
-# Exchange request token for authorized access token
-verifier  = self.request.get('oauth_verifier') # must fetch oauth_verifier from request
+request_token, request_token_secret = yahoo.get_request_token(data = { 'oauth_callback': "oob" })
 
-access_token  = oauthapp.get_access_token(request_token, verifier)
 
-# update access token
-oauthapp.token = access_token
-
-profile = oauthapp.getProfile()
-
-print profile
+print "Request Token:"
+print " - oauth_token = %s" % request_token
+print " - oauth_token_secret = %s" % request_token_secret
+auth_url = yahoo.get_authorize_url(request_token)
+print 'Visit this URL in your browser: ' + auth_url
+pin = raw_input('Enter PIN from browser: ')
+session = yahoo.get_auth_session(request_token, request_token_secret, method='POST', data={'oauth_verifier': pin})
